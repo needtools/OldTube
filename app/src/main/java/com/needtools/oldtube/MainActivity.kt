77d0ff887e7e15ae60edd8458d701c0e36ec6f40
Viktor <needtools@gmail.com>
2025-12-27 17:19:41 +0200

    Full project restore

--- app/src/main/java/com/needtools/oldtube/MainActivity.kt
+++ app/src/main/java/com/needtools/oldtube/MainActivity.kt
@@ -3,9 +3,6 @@ package com.needtools.oldtube
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.needtools.oldtube.ui.theme.OldTubeTheme
@@ -13,16 +10,15 @@ import com.needtools.oldtube.ui.theme.OldTubeTheme
 import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
-    private val LOG_TAG = "mainLog"
-    // 1. Стан, який ми будемо змінювати для примусової перекомпозиції
-    var shouldRefreshData by mutableStateOf(0)
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
             OldTubeTheme {
                 val navController = rememberNavController()
-                val refreshKey = shouldRefreshData
+
                 NavHost(
                     navController = navController,
                     startDestination = "youtube_screen"
